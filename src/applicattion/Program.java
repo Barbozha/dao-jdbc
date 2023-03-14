93c536e38cea0dad1bc02c961d0895c74fd03376
Barbozha <barbozha@gmail.com>
2023-03-14 16:42:05 -0300

    Entidade da Classe Departamento

---
+++ src/applicattion/Program.java
@@ -0,0 +1,17 @@
+package applicattion;
+
+import model.entites.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Nome Jorge Paulo
+		// Data: 14/03/2022
+		
+		Department dep = new Department(1, "Books");
+		
+		System.out.println(dep);
+
+	}
+
+}
