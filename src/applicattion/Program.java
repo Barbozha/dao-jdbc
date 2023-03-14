531248197a490dd3a0e1ce6bfc54a89184c9c97d
Barbozha <barbozha@gmail.com>
2023-03-14 18:07:21 -0300

    SellerDaoJDBC and DaoFactory

--- src/applicattion/Program.java
+++ src/applicattion/Program.java
@@ -3,6 +3,8 @@ package applicattion;
 import java.util.Date;
 import java.util.Locale;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entites.Department;
 import model.entites.Seller;
 
@@ -16,6 +18,8 @@ public class Program {
 		Department dep = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
