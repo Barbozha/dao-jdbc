4d0433bcf12139607025073166542f64bda40d1d
Barbozha <barbozha@gmail.com>
2023-03-15 13:59:02 -0300

    Implementacao do findById

--- src/applicattion/Program.java
+++ src/applicattion/Program.java
@@ -1,11 +1,9 @@
 package applicattion;
 
-import java.util.Date;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entites.Department;
 import model.entites.Seller;
 
 public class Program {
@@ -15,11 +13,10 @@ public class Program {
 		// Data: 14/03/2022
 		
 		Locale.setDefault(Locale.US);
-		Department dep = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
